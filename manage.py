7d8c128cd01287b5903e43bb2c4b7d37b9c2e962
pzf0000 <pzf0000@foxmail.com>
2019-05-04 00:00:42 +0800

    create django admin user before server start

--- manage.py
+++ manage.py
@@ -6,6 +6,23 @@ import sys
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Easy_Database_User.settings')
     try:
+        print("Try to create a django admin user.")
+        user_name = os.getenv('DJANGO_ADMIN_USERNAME')
+        user_pwd = os.getenv('DJANGO_ADMIN_PASSWORD')
+        user_email = os.getenv('DJANGO_ADMIN_USEREMAIL')
+        import django
+        django.setup()
+        if user_name is not None and user_pwd is not None and user_email is not  None:
+            from django.contrib.auth.models import User
+            user = User.objects.create(username=user_name, email=user_email, is_superuser=True, is_staff=True)
+            user.set_password(user_pwd)
+            user.save()
+        else:
+            print("The required parameters are not in the environment variable and the creation fails.")
+    except Exception as e:
+        print(e)
+        print("Create django admin user fail. Django server will launch without any admin user.")
+    try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
